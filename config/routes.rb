946c50743de895e93e5a56c1e0640e34007e2709
Aketa Yudai <yuutai1201@gmail.com>
2020-06-04 14:45:42 +0900

    フォロー、フォロー解除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get :about,        to: 'static_pages#about'
-  get :use_of_terms, to: 'static_pages#terms'
-  get 'signup',      to: 'users#new'
-  get    :login,     to: 'sessions#new'
-  post   :login,     to: 'sessions#create'
-  delete :logout,    to: 'sessions#destroy'
-  resources :users
+  get    :about,        to: 'static_pages#about'
+  get    :use_of_terms, to: 'static_pages#terms'
+  get    'signup',      to: 'users#new'
+  get    :login,        to: 'sessions#new'
+  post   :login,        to: 'sessions#create'
+  delete :logout,       to: 'sessions#destroy'
+
+  resources :relationships, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :courses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_153053) do
+ActiveRecord::Schema.define(version: 2020_06_03_082649) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2020_06_02_153053) do
     t.string "picture"
     t.index ["user_id", "created_at"], name: "index_courses_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_courses_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
