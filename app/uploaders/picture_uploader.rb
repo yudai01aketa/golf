9c168c5beb3a35e60c00c18cc92f2721f1233088
Aketa Yudai <yuutai1201@gmail.com>
2020-07-03 15:14:08 +0900

    AWS_S3に画像保存

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,9 +1,14 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :file
-  # storage :fog
+  include CarrierWave::MiniMagick
 
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
