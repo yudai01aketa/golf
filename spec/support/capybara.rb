1567fefd4dc8fdb9e3e6756235f6553c1b61e824
Aketa Yudai <yuutai1201@gmail.com>
2020-07-14 18:41:25 +0900

    capybara.rb変更

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,14 +1,15 @@
-# デリート時に出てくるOKボタンのクリックが可能
+#変更後
 Capybara.register_driver :chrome_headless do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
+
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--window-size=1400,1400')
+
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-# システムスペック用の設定
 Capybara.javascript_driver = :chrome_headless
 
 RSpec.configure do |config|
@@ -17,6 +18,14 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    driven_by :chrome_headless
+    if ENV["SELENIUM_DRIVER_URL"].present?
+      driven_by :selenium, using: :chrome, options: {
+        browser: :remote,
+        url: ENV.fetch("SELENIUM_DRIVER_URL"),
+        desired_capabilities: :chrome
+      }
+    else
+      driven_by :selenium_chrome_headless
+    end
   end
 end
