9ecc95d185afac6ab0d7649e7e12290186227eb9
Aketa Yudai <yuutai1201@gmail.com>
2020-07-16 18:21:24 +0900

    circleci

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,12 +1,10 @@
 Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-
+  options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
+  options.add_argument('--disable-gpu')
+  chrome_options.add_argument('--no-sandbox')         
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  driver = Selenium::WebDriver.for :chrome, options: options
 end
 
 Capybara.javascript_driver = :chrome_headless
