ca1e600e1f06214a3924ccc57afd71847d7355e6
Aketa Yudai <yuutai1201@gmail.com>
2020-08-19 10:19:51 +0900

    楽天GORAのコースを検索一覧機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,7 +84,7 @@ class User < ApplicationRecord
 
   # 現在のユーザーがフォローされていたらtrueを返す
   def followed_by?(other_user)
-    followers.include?(other_user)
+    followers.exists?(other_user)
   end
 
   # コースをお気に入りに登録する
@@ -99,7 +99,7 @@ class User < ApplicationRecord
 
   # 現在のユーザーがお気に入り登録してたらtrueを返す
   def favorite?(course)
-    !Favorite.find_by(user_id: id, course_id: course.id).nil?
+    Favorite.exists?(user_id: id, course_id: course.id)
   end
 
   # コースをリストに登録する
@@ -114,7 +114,7 @@ class User < ApplicationRecord
 
   # 現在のユーザーがリスト登録してたらtrueを返す
   def list?(course)
-    !List.find_by(course_id: course.id, from_user_id: id).nil?
+    List.exists?(course_id: course.id, from_user_id: id)
   end
 
   def self.guest
---
+++ spec/requests/rakuten_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "楽天GORA", type: :request do
+  context "通知一覧ページの表示" do
+    it "レスポンスが正常に表示されること" do
+      get rakuten_index
+      expect(response).to render_template('rakuten/index')
+    end
+  end
+end
