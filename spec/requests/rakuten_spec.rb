0b41034dceb446353b7affeb5935729d4c201738
Aketa Yudai <yuutai1201@gmail.com>
2020-08-19 11:05:10 +0900

    楽天GORAのコースを検索一覧機能追加

--- spec/requests/rakuten_spec.rb
+++ spec/requests/rakuten_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "楽天GORA", type: :request do
   context "通知一覧ページの表示" do
     it "レスポンスが正常に表示されること" do
-      get rakuten_index
-      expect(response).to render_template('rakuten')
+      get rakuten
+      expect(response).to render_template('rakuten_index')
     end
   end
 end
