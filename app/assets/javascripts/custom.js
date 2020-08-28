122c43fa77a9d2023e592f4acf4fd57412fb7a83
Aketa Yudai <yuutai1201@gmail.com>
2020-08-28 23:36:38 +0900

    各ページデザイン変更

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -6,3 +6,20 @@ $(function() {
       nav.toggleClass('toggle');
    });
 });
+
+//コース個別ページの<続きを読む>//
+$(function() {
+  var textHeight = $('.text').height(),
+    lineHeight = parseFloat($('.text').css('line-height')),
+    lineNum = 4,
+    textNewHeight = lineHeight * lineNum;
+  if (textHeight > textNewHeight) {
+    $('.text').css('height', textNewHeight);
+    $('.readmore-btn').show();
+    $('.readmore-btn').click(function() {
+      $(this).hide();
+      $('.text').css('height', textHeight);
+      return false
+    });
+  };
+});
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -19,7 +19,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # 画像サイズ設定
   # 詳細表示用：400 * 400の正方形に整形
   version :thumb400 do
-    process resize_and_pad(400, 400) # background = :transparent, gravity = 'Center'
+    process resize_to_fill(400, 400, "Center") # background = :transparent, gravity = 'Center'
   end
 
   # 一覧表示用：200 * 200の正方形に中央から切り抜き
