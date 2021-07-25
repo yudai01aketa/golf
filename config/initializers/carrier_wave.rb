749312d4bcab4009747a9b25cd286ea3fb6c66e8
yudai01aketa <yuutai1201@gmail.com>
2021-07-26 00:52:19 +0900

    --amend

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,20 +3,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['S3_REGION']
-  }
-
-    case Rails.env
-    when 'development'
-        config.fog_directory  = ENV['S3_BUCKET']
-        config.asset_host = ENV['AWS_BUCKET_URL']
-    when 'production'
-        config.fog_directory  = ENV['S3_BUCKET']
-        config.asset_host = ENV['AWS_BUCKET_URL']
-    end
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false if Rails.env.test?
+  elsif Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV['S3_REGION']
+    }
+    config.storage = :fog
+    config.fog_directory = ENV['S3_BUCKET']
+    config.asset_host = ENV['AWS_BUCKET_URL']
+  end
 end
